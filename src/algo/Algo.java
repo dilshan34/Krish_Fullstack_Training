013192ed49f50718d3fcec38a6824b75d13dff7b
dilshan34 <dilshansachinthana34@gmail.com>
2022-04-11 00:31:34 +0530

    BFS

---
+++ src/algo/Algo.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algo;
+
+/**
+ *
+ * @author Breeze
+ */
+public class Algo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        BFS g = new BFS(4);
+ 
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+ 
+        System.out.println("Following is Breadth First Traversal "+
+                           "(starting from vertex 2)");
+ 
+        g.BFS(2);
+    }
+    
+}

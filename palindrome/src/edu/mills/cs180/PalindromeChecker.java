d29953d4f7781a2c5b3ab1887552933e002141d8
Dilshan Sachinthana <dilshansachinthana34@gmail.com>
2022-07-26 19:53:22 +0530

    palindrome
    palindrome

---
+++ palindrome/src/edu/mills/cs180/PalindromeChecker.java
@@ -0,0 +1,21 @@
+package edu.mills.cs180;
+
+/**
+ * A static utility class for testing whether strings are palindromes.
+ *
+ * @author Ellen Spertus
+ */
+public class PalindromeChecker {
+    private PalindromeChecker() {}
+
+    /**
+     * Checks whether the given string is a palindrome. A string is considered a palindrome if it
+     * reads the same forwards and backwards once all non-alphanumeric characters are removed.
+     *
+     * @param s the string to check
+     * @return true if the string is a palindrome, false otherwise
+     */
+    public static boolean isPalindrome(String s) {
+        return false;
+    }
+}
